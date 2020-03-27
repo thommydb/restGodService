69bc29459e299d571d0eeb4fe1a051bd59dd2e98
tomas.deblas <tomas.deblas@vass.es>
2020-03-27 16:55:11 +0100

    create method getGod in controller

--- src/main/java/com/example/demo/GodController.java
+++ src/main/java/com/example/demo/GodController.java
@@ -1,5 +1,22 @@
 package com.example.demo;
 
-public class GodController {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GodController {
+	
+	private final GodService godService;
+	
+	public GodController(GodService godService) {
+		this.godService = godService;
+	}
+
+	@GetMapping("/greeting")
+	public List<String> getGod(@RequestParam(value = "nationality", defaultValue = "World") String nationality) {
+		return godService.getGod(nationality);
+	}
 }
