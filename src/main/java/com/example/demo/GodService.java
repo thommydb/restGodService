3a961ed7341668c90b9c4edc48677af84849068d
tomas.deblas <tomas.deblas@vass.es>
2020-03-27 16:49:36 +0100

    getGod method of service GodService

--- src/main/java/com/example/demo/GodService.java
+++ src/main/java/com/example/demo/GodService.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class GodService {
 
+	public List<String> getGod(String nationality){
+		return Arrays.asList("Zeus", "Afrodita", "Apolo");
+	}
 }
